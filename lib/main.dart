bfeda54215e6b5b884034aa314021d216274515d
ikyrie <mah.c5r@gmail.com>
2025-02-04 17:29:13 -0300

    Adicionando o printer settings store nos providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:panucci_ristorante/screens/home.dart';
 import 'package:panucci_ristorante/store/carrinho_store.dart';
+import 'package:panucci_ristorante/store/printer_settings_store.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -14,7 +15,10 @@ const PanucciDelivery({ Key? key }) : super(key: key);
   Widget build(BuildContext context){
     return MaterialApp(
       theme: ThemeData.from(colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 83, 83), ), useMaterial3: true),
-      home: Provider(create: (_) => CarrinhoStore(), child: Home()),
+      home: MultiProvider(providers: <Provider>[
+        Provider<CarrinhoStore>(create: (_) => CarrinhoStore()),
+        Provider<PrinterSettingsStore>(create: (_) => PrinterSettingsStore()),
+      ],child: Home(),),
     );
   }
 }
