0723958e75a522050f75d4d15286a1ab1532573f
ikyrie <mah.c5r@gmail.com>
2025-02-11 11:16:37 -0300

    video2.3

--- lib/screens/checkout.dart
+++ lib/screens/checkout.dart
@@ -80,7 +80,16 @@ class Checkout extends StatelessWidget {
                       List<int> bytes = [];
                       final profile = await CapabilityProfile.load();
                       final generator = Generator(PaperSize.mm58, profile);
-                      bytes += generator.text("Um item da comanda");
+                      bytes += generator.text("Um item da comanda",linesAfter: 2, styles: PosStyles(align: PosAlign.center, height: PosTextSize.size1));
+                      bytes += generator.reset();
+                      bytes += generator.text("Um item da comanda",linesAfter: 2, styles: PosStyles(height: PosTextSize.size2));
+                      bytes += generator.reset();
+                      bytes += generator.row([PosColumn(text: "Item 1", width: 6, styles: PosStyles(align: PosAlign.left)), PosColumn(text: "Item 2", width: 6, styles: PosStyles(align: PosAlign.right))]);
+                      bytes += generator.reset();
+                      bytes += generator.text("Um texto em negrito", styles: PosStyles(fontType: PosFontType.fontB));
+                      bytes += generator.reset();
+                      bytes += generator.qrcode("Um site qualquer.com", size: QRSize.size8);
+                      bytes += generator.reset();
                       await ReceiptPrintingService.printReceipt(bytes);
                     },
                   ),
