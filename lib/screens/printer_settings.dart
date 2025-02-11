7e0694ad2e91e850dd7caa3c6f0f345d746de04a
ikyrie <mah.c5r@gmail.com>
2025-02-11 17:04:46 -0300

    video4.1

--- lib/screens/printer_settings.dart
+++ lib/screens/printer_settings.dart
@@ -1,3 +1,4 @@
+import 'package:esc_pos_utils_plus/esc_pos_utils_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:panucci_ristorante/components/custom_buttons.dart';
 
@@ -6,6 +7,9 @@ class PrinterSettings extends StatelessWidget {
 
   final TextEditingController paperSizeController = TextEditingController();
   final TextEditingController fontSizeController = TextEditingController();
+
+  PosTextSize textSize = PosTextSize.size1;
+  PaperSize paperSize = PaperSize.mm58;
 
   @override
   Widget build(BuildContext context) {
@@ -33,9 +37,11 @@ class PrinterSettings extends StatelessWidget {
               SizedBox(height: 40,),
               Text("Selecione as configurações básicas para a sua impressão:"),
               SizedBox(height: 24,),
-              DropdownMenu<dynamic>(
+              DropdownMenu<PaperSize>(
                 controller: paperSizeController,
-                onSelected: (value) => {},
+                onSelected: (value) => {
+                  paperSize = value!
+                },
                 inputDecorationTheme: InputDecorationTheme(
                   contentPadding: const EdgeInsets.all(16),
                   border: OutlineInputBorder(
@@ -44,17 +50,19 @@ class PrinterSettings extends StatelessWidget {
                 ),
                 width: 200,
                 label: Text("Tamanho do papel"),
-                initialSelection: 1,
+                initialSelection: PaperSize.mm58,
                 dropdownMenuEntries: [
-                  DropdownMenuEntry(value: 1, label: "58mm"),
-                  DropdownMenuEntry(value: 2, label: "72mm"),
-                  DropdownMenuEntry(value: 3, label: "80mm"),
+                  DropdownMenuEntry(value: PaperSize.mm58, label: "58mm"),
+                  DropdownMenuEntry(value: PaperSize.mm72, label: "72mm"),
+                  DropdownMenuEntry(value: PaperSize.mm80, label: "80mm"),
                 ],
               ),
               SizedBox(height: 24,),
-              DropdownMenu<dynamic>(
+              DropdownMenu<PosTextSize>(
                 controller: fontSizeController,
-                onSelected: (value) => {},
+                onSelected: (value) => {
+                  textSize = value!
+                },
                 inputDecorationTheme: InputDecorationTheme(
                   contentPadding: const EdgeInsets.all(16),
                   border: OutlineInputBorder(
@@ -63,16 +71,16 @@ class PrinterSettings extends StatelessWidget {
                 ),
                 width: 200,
                 label: Text("Tamanho da fonte"),
-                initialSelection: 1,
+                initialSelection: PosTextSize.size1,
                 dropdownMenuEntries: [
-                  DropdownMenuEntry(value: 1, label: "1"),
-                  DropdownMenuEntry(value: 2, label: "2"),
-                  DropdownMenuEntry(value: 3, label: "3"),
-                  DropdownMenuEntry(value: 4, label: "4"),
-                  DropdownMenuEntry(value: 5, label: "5"),
-                  DropdownMenuEntry(value: 6, label: "6"),
-                  DropdownMenuEntry(value: 7, label: "7"),
-                  DropdownMenuEntry(value: 8, label: "8"),
+                  DropdownMenuEntry(value: PosTextSize.size1, label: "1"),
+                  DropdownMenuEntry(value: PosTextSize.size2, label: "2"),
+                  DropdownMenuEntry(value: PosTextSize.size3, label: "3"),
+                  DropdownMenuEntry(value: PosTextSize.size4, label: "4"),
+                  DropdownMenuEntry(value: PosTextSize.size5, label: "5"),
+                  DropdownMenuEntry(value: PosTextSize.size6, label: "6"),
+                  DropdownMenuEntry(value: PosTextSize.size7, label: "7"),
+                  DropdownMenuEntry(value: PosTextSize.size8, label: "8"),
                 ],
               ),
               Flexible(
